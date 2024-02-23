4600ef0ed4baf4d4491f4292b285ee8e0ea39407
AReeme <areeme@student.neumont.edu>
2024-02-22 20:14:40 -0700

    Black Jack Game Logic Completed

---
+++ Assets/Global/Scripts/Black Jack/BlackJackGameManager.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = System.Random;
+
+public class BlackJackGameManager : MonoBehaviour
+{
+    //Game Buttons 
+    private Button dealBtn;
+    private Button hitBtn;
+    private Button standBtn;
+    private Button betBtn;
+
+    //Player Hand
+    private Card[] playerHand;
+
+    //Dealer Hand
+    private Card[] dealerHand;
+
+    //Deck
+    private List<Card> deck;
+
+    void Start()
+    {
+        // Click Listeners 
+        dealBtn.onClick.AddListener(() => DealClicked());
+        hitBtn.onClick.AddListener(() => HitClicked());
+        standBtn.onClick.AddListener(() => StandClicked());
+        betBtn.onClick.AddListener(() => BetClicked());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void BetClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void StandClicked()
+    {
+        DealerTurn();
+    }
+
+    private void HitClicked()
+    {
+        if (deck == null || deck.Count == 0)
+        {
+            Debug.Log("Deck is Empty/Null");
+        }
+
+        Card drawnCard = deck[0];
+        deck.RemoveAt(0);
+
+        Array.Resize(ref playerHand, playerHand.Length + 1);
+        playerHand[playerHand.Length - 1] = drawnCard;
+
+        int handValue = GetCardValues(playerHand);
+        if (handValue > 21) 
+        {
+            Debug.Log("Player Lost");
+            DetermineWinner();
+        }
+    }
+
+    private void DealClicked()
+    {
+        if (deck == null || deck.Count == 0)
+        {
+            Debug.Log("Deck is Empty/Null");
+        }
+        
+        deck = deck.Shuffle();
+
+        if (playerHand == null)
+        {
+            playerHand = new Card[2];
+        }
+
+        if (dealerHand == null)
+        {
+            dealerHand = new Card[2];
+        }
+
+        for (int i = 0; i < 2; i++) 
+        {
+            playerHand[i] = deck[0];
+            deck.RemoveAt(i);
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            dealerHand[i] = deck[0];
+            deck.RemoveAt(i);
+        }
+    }
+
+    private int GetCardValues(Card[] hand)
+    {
+        int value = 0;
+        int aces = 0;
+
+        foreach (Card card in hand) 
+        { 
+            int cardValue = card.GetBlackJackValue();
+            value += cardValue;
+
+            if (card.rank == Card.Rank.Ace)
+            {
+                aces++;
+            }
+
+            while (value > 21 && aces > 0)
+            {
+                value -= 10;
+                aces--;
+            }
+        }
+
+        return value;
+    }
+
+    private void DealerTurn()
+    {
+        if (GetCardValues(dealerHand) < 17)
+        {
+            Card drawnCard = deck[0];
+            deck.RemoveAt(0);
+            Array.Resize(ref dealerHand, dealerHand.Length + 1);
+            dealerHand[dealerHand.Length - 1] = drawnCard;
+        }
+
+        DetermineWinner();
+    }
+
+    private void DetermineWinner() 
+    {
+        int playerValue = GetCardValues(playerHand);
+        int dealerValue = GetCardValues(dealerHand);
+
+        if (playerValue > 21 && dealerValue < 21)
+        {
+            Debug.Log("Player Busts. Dealer Wins");
+        } else if (playerValue < 21 && dealerValue > 21) 
+        {
+            Debug.Log("Dealer Busts. Player Wins");
+        } else if (playerValue > dealerValue)
+        {
+            Debug.Log("Player Wins.");
+        } else if (dealerValue > playerValue)
+        {
+            Debug.Log("Dealer Wins");
+        }
+        else
+        {
+            Debug.Log("It's a Draw");
+        }
+    }
+}
--- Assets/Global/Scripts/Card/Deck.cs
+++ Assets/Global/Scripts/Card/Deck.cs
@@ -20,7 +20,7 @@ public class Card
 	}
 
 	Suit suit;
-	Rank rank;
+	public Rank rank;
 
 	public Card(Suit suit, Rank rank)
 	{
@@ -31,6 +31,38 @@ public class Card
 	public Card(Rank rank)
 	{
 		this.rank = rank;
+	}
+
+	public int GetBlackJackValue()
+	{
+		switch (rank)
+		{
+			case Rank.Ace:
+				return 11;
+			case Rank.Two:
+				return 2;
+			case Rank.Three:
+				return 3;
+			case Rank.Four:
+				return 4;
+			case Rank.Five:
+				return 5;
+			case Rank.Six:
+				return 6;
+			case Rank.Seven:
+				return 7;
+			case Rank.Eight:
+				return 8;
+			case Rank.Nine:
+				return 9;
+			case Rank.Ten:
+			case Rank.Jack:
+			case Rank.Queen:
+			case Rank.King:
+				return 10;
+			default:
+				return 0;
+		}
 	}
 }
 
