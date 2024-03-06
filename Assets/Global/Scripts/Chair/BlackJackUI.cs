3ddbbe8c2729c2e14e70f0c31df6cf1f2b4e3716
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2024-03-05 21:23:35 -0700

    Updated BlackJack UI

--- Assets/Global/Scripts/Chair/BlackJackUI.cs
+++ Assets/Global/Scripts/Chair/BlackJackUI.cs
@@ -8,7 +8,6 @@ public class BlackJackUI : MonoBehaviour
     [SerializeField] Button dealBTN;
     [SerializeField] Button standBTN;
     [SerializeField] Button hitBTN;
-    [SerializeField] Button DealBTN;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +20,13 @@ public class BlackJackUI : MonoBehaviour
         standBTN.gameObject.SetActive(true);
         hitBTN.gameObject.SetActive(true);
         dealBTN.gameObject.SetActive(false);
-        dealBTN.gameObject.SetActive(false);
     }
 
     public void Stand()
     {
         hitBTN.gameObject.SetActive(false);
         standBTN.gameObject.SetActive(false);
-        Invoke(nameof(UIReset), 3);
+        UIReset();
     }
 
 	public void UIReset()
